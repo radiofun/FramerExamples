e4dd200db1e66f81a149e1e5bb5d707f1786d0ea
Josh Puckett <mith.puck@gmail.com>
2014-05-10 11:56:14 -0700

    updated Layer Properties example

--- Layer Properties.framer/app.coffee
+++ Layer Properties.framer/app.coffee
@@ -1,27 +1,28 @@
 # Changing layer properties
 
-# A new layer gets a set of default properties, a width and 
+# A new layer has a set of default properties: a width and 
 # height of 100 and a blue color. You can change these, see
 # the example Framer Defaults.
 
 layerA = new Layer
 
-# A layer has a whole set of properties like width, height,
+# A layer has many properties like width, height,
 # backgroundColor, opacity etc. You can find them all in the
 # documentation here: http://www.framerjs.com
 
 # You can set any of them on creation like this
-layerB = new Layer width:50, height:50, x:0, y:150, backgroundColor:"red"
+layerB = new Layer width:50, height:50, x:50, y:150, backgroundColor:"red"
 
 # You can also set them at any time afterwards
-layerC = new Layer width:50, height:50, x:0, y:250, backgroundColor:"green"
-layerC.rotation = 20
+layerC = new Layer width:50, height:50, x:50, y:250, backgroundColor:"green"
+layerC.rotation = 10
 
-# Here are some more properties to play with
-layerC = new Layer width:100, height:100, x:110, y:350, backgroundColor:"orange"
-
-layerC.scale = 1.2
-layerC.opacity = 0.5
-layerC.rotationX = 10
-layerC.rotationY = 30
-layerC.originX = 0.2
+# Here are some more properties to play with, and you can
+# also see how to set multiple properties at once.
+layerD = new Layer width:100, height:100, x:150, y:350, backgroundColor:"orange"
+layerD.properties = 
+	opacity: .50
+	scale: 1.2
+	rotationX: 45
+	rotationZ: 45
+	blur: 5
--- Layer Properties.framer/framer/framer.js
+++ Layer Properties.framer/framer/framer.js
@@ -88,7 +88,7 @@ exports.Animation = (function(_super) {
   };
 
   Animation.prototype._parseAnimatorOptions = function() {
-    var animatorClass, i, k, parsedCurve, value, _i, _j, _len, _len1, _ref, _ref1, _results;
+    var animatorClass, parsedCurve;
     animatorClass = this._animatorClass();
     parsedCurve = Utils.parseFunction(this.options.curve);
     if (animatorClass === LinearAnimator || animatorClass === BezierCurveAnimator) {
@@ -97,32 +97,19 @@ exports.Animation = (function(_super) {
     if (parsedCurve.args.length) {
       if (animatorClass === BezierCurveAnimator) {
         this.options.curveOptions.values = parsedCurve.args.map(function(v) {
-          return parseFloat(v) || 0;
+          return parseFloat(v);
         });
       }
       if (animatorClass === SpringRK4Animator) {
-        _ref = ["tension", "friction", "velocity"];
-        for (i = _i = 0, _len = _ref.length; _i < _len; i = ++_i) {
-          k = _ref[i];
-          value = parseFloat(parsedCurve.args[i]);
-          if (value) {
-            this.options.curveOptions[k] = value;
-          }
-        }
+        this.options.curveOptions.tension = parseFloat(parsedCurve.args[0]);
+        this.options.curveOptions.friction = parseFloat(parsedCurve.args[1]);
+        this.options.curveOptions.velocity = parseFloat(parsedCurve.args[2]);
       }
       if (animatorClass === SpringDHOAnimator) {
-        _ref1 = ["stiffness", "damping", "mass", "tolerance"];
-        _results = [];
-        for (i = _j = 0, _len1 = _ref1.length; _j < _len1; i = ++_j) {
-          k = _ref1[i];
-          value = parseFloat(parsedCurve.args[i]);
-          if (value) {
-            _results.push(this.options.curveOptions[k] = value);
-          } else {
-            _results.push(void 0);
-          }
-        }
-        return _results;
+        this.options.curveOptions.stiffness = parseFloat(parsedCurve.args[0]);
+        this.options.curveOptions.damping = parseFloat(parsedCurve.args[1]);
+        this.options.curveOptions.mass = parseFloat(parsedCurve.args[2]);
+        return this.options.curveOptions.tolerance = parseFloat(parsedCurve.args[3]);
       }
     }
   };
@@ -273,12 +260,12 @@ AnimationLoop = {
       }
     }
     AnimationLoop._time = time;
+    window.requestAnimationFrame(AnimationLoop._tick);
     for (_j = 0, _len1 = removeAnimators.length; _j < _len1; _j++) {
       animator = removeAnimators[_j];
       AnimationLoop.remove(animator);
       animator.emit("end");
     }
-    window.requestAnimationFrame(AnimationLoop._tick);
   },
   add: function(animator) {
     if (animator.hasOwnProperty(AnimationLoopIndexKey)) {
@@ -289,7 +276,7 @@ AnimationLoop = {
     return AnimationLoop._start();
   },
   remove: function(animator) {
-    AnimationLoop._animators = _.without(AnimationLoop._animators, animator);
+    AnimationLoop._animators.splice(animator[AnimationLoop._animators], 1);
     return animator.emit("stop");
   }
 };
@@ -375,15 +362,14 @@ exports.BezierCurveAnimator = (function(_super) {
   }
 
   BezierCurveAnimator.prototype.setup = function(options) {
-    if (_.isString(options) && BezierCurveDefaults.hasOwnProperty(options.toLowerCase())) {
-      options = {
-        values: BezierCurveDefaults[options.toLowerCase()]
-      };
-    }
-    if (_.isArray(options) && options.length === 4) {
-      options = {
-        values: options
-      };
+    var optionValues, _ref1;
+    optionValues = (_ref1 = options.values) != null ? _ref1.toLowerCase() : void 0;
+    if (_.isString(optionValues)) {
+      if (BezierCurveDefaults.hasOwnProperty(optionValues)) {
+        options.values = BezierCurveDefaults[optionValues];
+      } else {
+        delete options.values;
+      }
     }
     this.options = Utils.setDefaultProperties(options, {
       values: BezierCurveDefaults["ease-in-out"],
@@ -550,7 +536,6 @@ exports.SpringDHOAnimator = (function(_super) {
       mass: 0.2,
       time: null
     });
-    console.log("SpringDHOAnimator.options", this.options, options);
     this._time = 0;
     this._value = 0;
     return this._velocity = this.options.velocity;
@@ -602,7 +587,7 @@ exports.SpringRK4Animator = (function(_super) {
   SpringRK4Animator.prototype.setup = function(options) {
     this.options = Utils.setDefaultProperties(options, {
       tension: 500,
-      friction: 10,
+      friction: 25,
       velocity: 0,
       tolerance: 1 / 10000,
       time: null
