75939f573877caac257dd9c486ea2509b4c70c93
benjaminnathan <benjamin@benjaminnathan.nl>
2014-05-12 19:42:46 +0200

    CSS

---
+++ FramerExamplesSite/.build/static/js/example.js
@@ -0,0 +1,27 @@
+(function() {
+  var getParameterByName;
+
+  getParameterByName = function(name) {
+    var regex, results;
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
+    results = regex.exec(location.search);
+    if (results == null) {
+      return "";
+    } else {
+      return decodeURIComponent(results[1].replace(/\+/g, " "));
+    }
+  };
+
+  $(document).ready(function() {
+    var exampleName;
+    exampleName = getParameterByName("name");
+    $("head").append($("<base href=\"/static/examples/" + exampleName + "/\">"));
+    return $.getScript("framer/framer.js", function() {
+      return $.getScript("app.js", function(err, data) {
+        return console.log(data);
+      });
+    });
+  });
+
+}).call(this);
