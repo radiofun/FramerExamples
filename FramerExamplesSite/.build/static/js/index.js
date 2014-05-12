25be01fcfef47ac463dbd7eb989881fbd7ab730b
Koen Bok <koen@madebysofa.com>
2014-05-12 19:27:41 +0200

    boom

--- FramerExamplesSite/.build/static/js/index.js
+++ FramerExamplesSite/.build/static/js/index.js
@@ -1,10 +1,20 @@
 (function() {
+  var showExample;
+
+  showExample = function(exampleName) {
+    $("#code").attr("src", "code.html?name=" + exampleName);
+    return $("#example").attr("src", "example.html?name=" + exampleName);
+  };
+
   $(document).ready(function() {
+    var exampleName;
+    exampleName = window.location.hash.slice(1);
+    if (exampleName) {
+      showExample(exampleName);
+    }
     return $("a").click(function() {
-      var exampleName;
       exampleName = $(this).attr("href").slice(1);
-      $("#code").attr("src", "code.html?name=" + exampleName);
-      return $("#example").attr("src", "example.html?name=" + exampleName);
+      return showExample(exampleName);
     });
   });
 
