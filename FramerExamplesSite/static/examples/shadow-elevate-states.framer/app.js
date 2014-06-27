6832d5fe734346d4015917ba3b2d48726eafefe3
Koen Bok <koen@madebysofa.com>
2014-06-27 15:45:36 +0200

    Shadow examples

---
+++ FramerExamplesSite/static/examples/shadow-elevate-states.framer/app.js
@@ -0,0 +1,60 @@
+var background, layer;
+
+background = new BackgroundLayer({
+  backgroundColor: "rgba(77, 208, 225, 1.00)"
+});
+
+layer = new Layer({
+  x: 100,
+  y: 100,
+  width: 200,
+  height: 200,
+  backgroundColor: "white"
+});
+
+layer.center();
+
+layer.shadowColor = "rgba(0, 0, 0, 0.2)";
+
+layer.style = {
+  "color": "black",
+  "line-height": "200px",
+  "text-align": "center"
+};
+
+/* These are loosely based on the Google Material specs */
+
+layer.states.add({
+  elevatedZ1: {
+    shadowY: 2,
+    shadowBlur: 10
+  },
+  elevatedZ2: {
+    shadowY: 6,
+    shadowBlur: 20
+  },
+  elevatedZ3: {
+    shadowY: 12,
+    shadowBlur: 15
+  },
+  elevatedZ4: {
+    shadowY: 16,
+    shadowBlur: 28
+  },
+  elevatedZ5: {
+    shadowY: 27,
+    shadowBlur: 24
+  }
+});
+
+layer.states.animationOptions = {
+  curve: "ease-in",
+  time: 0.15
+};
+
+layer.on(Events.Click, function() {
+  layer.states.next();
+  return layer.html = layer.states.current;
+});
+
+layer.html = "Click me";
