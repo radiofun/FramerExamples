f647b8b2341e6674518fb173cc915b7147e17a63
benjaminnathan <benjamin@benjaminnathan.nl>
2014-07-03 09:39:07 +0200

    Final tweaks of nab-fix

--- FramerExamplesSite/static/js/index.coffee
+++ FramerExamplesSite/static/js/index.coffee
@@ -1,42 +1,49 @@
-showExample = (exampleName) ->
-
-	if ga?
-		ga("send", "pageview", "/examples/#{exampleName}")
-
-	$("#code").attr "src", "code.html?name=#{exampleName}"
-	$("#example").attr "src", "example.html?name=#{exampleName}"
-	$("a.download").attr "href", "/static/examples/#{exampleName}.zip"
-
+$(window).load ->
+	loadExample = (loadExampleName) ->
+		if ga?
+			ga("send", "pageview", "/examples/#{loadExampleName}")
 	
+		$("#code").attr "src", "code.html?name=#{loadExampleName}"
+		$("#example").attr "src", "example.html?name=#{loadExampleName}"
+		$("a.download").attr "href", "/static/examples/#{loadExampleName}.zip"
+	
+	
+	loadExampleName = window.location.hash[1..]
+	loadExample loadExampleName	
+
+
 $(document).ready ->
+		
+	showExample = (exampleName) ->
+		if ga?
+			ga("send", "pageview", "/examples/#{exampleName}")
 	
-	exampleName = window.location.hash
-
-	if not exampleName
-		window.location.hash = exampleName
-
-	showExample exampleName
-
-	$(".navigation ul li a").each ->
-		if $(@).attr("href")[1..] == exampleName
-			$(@).addClass "active"
-
+		$("#code").attr "src", "code.html?name=#{exampleName}"
+		$("#example").attr "src", "example.html?name=#{exampleName}"
+		$("a.download").attr "href", "/static/examples/#{exampleName}.zip"
+			
+	if not window.location.hash[1..]
+		window.location.hash = "animation-basics.framer"
+		loadExample "animation-basics.framer"	
+			
 	$(".navigation ul li a").click ->
-
+	
+		exampleName = $(@).attr("href")[1..]
+		showExample exampleName	
+		
 		$(".navigation ul li").removeClass "active"
 		$(@).parent().addClass "active"
 
-		exampleName = $(@).attr("href")[1..]
-		showExample exampleName
-		
 		$(".navigation").removeClass "appear"
 		$('#topbar').removeClass "active"
-
-
+			
+		
 	$('#topbar img').click ->
 	    $(".navigation").toggleClass "appear"
 	    $('#topbar').toggleClass "active"
-		    
+	    
+	 
+	    
 		
 
 
--- FramerExamplesSite/static/js/index.js
+++ FramerExamplesSite/static/js/index.js
@@ -1,32 +1,38 @@
 (function() {
-  var showExample;
-
-  showExample = function(exampleName) {
-    if (typeof ga !== "undefined" && ga !== null) {
-      ga("send", "pageview", "/examples/" + exampleName);
-    }
-    $("#code").attr("src", "code.html?name=" + exampleName);
-    $("#example").attr("src", "example.html?name=" + exampleName);
-    return $("a.download").attr("href", "/static/examples/" + exampleName + ".zip");
-  };
+  $(window).load(function() {
+    var loadExample, loadExampleName;
+    loadExample = function(loadExampleName) {
+      if (typeof ga !== "undefined" && ga !== null) {
+        ga("send", "pageview", "/examples/" + loadExampleName);
+      }
+      $("#code").attr("src", "code.html?name=" + loadExampleName);
+      $("#example").attr("src", "example.html?name=" + loadExampleName);
+      return $("a.download").attr("href", "/static/examples/" + loadExampleName + ".zip");
+    };
+    loadExampleName = window.location.hash.slice(1);
+    return loadExample(loadExampleName);
+  });
 
   $(document).ready(function() {
-    var exampleName;
-    exampleName = window.location.hash;
-    if (!exampleName) {
-      window.location.hash = exampleName;
-    }
-    showExample(exampleName);
-    $(".navigation ul li a").each(function() {
-      if ($(this).attr("href").slice(1) === exampleName) {
-        return $(this).addClass("active");
+    var showExample;
+    showExample = function(exampleName) {
+      if (typeof ga !== "undefined" && ga !== null) {
+        ga("send", "pageview", "/examples/" + exampleName);
       }
-    });
+      $("#code").attr("src", "code.html?name=" + exampleName);
+      $("#example").attr("src", "example.html?name=" + exampleName);
+      return $("a.download").attr("href", "/static/examples/" + exampleName + ".zip");
+    };
+    if (!window.location.hash.slice(1)) {
+      window.location.hash = "animation-basics.framer";
+      loadExample("animation-basics.framer");
+    }
     $(".navigation ul li a").click(function() {
-      $(".navigation ul li").removeClass("active");
-      $(this).parent().addClass("active");
+      var exampleName;
       exampleName = $(this).attr("href").slice(1);
       showExample(exampleName);
+      $(".navigation ul li").removeClass("active");
+      $(this).parent().addClass("active");
       $(".navigation").removeClass("appear");
       return $('#topbar').removeClass("active");
     });
